c0ac53e493e770cdab2e47ae59498123397f7f73
Ozgur Guzeldereli <ozgur.guzeldereli.24@ucl.ac.uk>
2024-11-25 21:27:09 +0000

    Added some basics

--- src/esp32/esp32.ino
+++ src/esp32/esp32.ino
@@ -1,4 +1,5 @@
 #include "src/connection/connection.h"
+#include "src/i2c/i2c.h"
 
 // Replace with your eduroam credentials
 char* ssid = "eduroam";
@@ -6,12 +7,23 @@ char* USER = "zcabogu@ucl.ac.uk";
 char* PASS = "Muhtesem3011";
 
 void setup() {
-  Serial.begin(115200);
-  bool connected = wifi_begin_enterprise(ssid, USER, PASS);
-  if (connected) {
-    Serial.println("Hello WiFi");
-  }
+    Serial.begin(115200); // initialize serial connection
+    i2c_init_listener(); // initialize arduino connection
+
+    bool connected = false;
+    do 
+    {
+        Serial.println("Attempting WiFi connection...");
+        connected = wifi_begin_enterprise(ssid, USER, PASS); // connect to eduroam
+    }
+    while(!connected);
+
+    configTime(0, 0, "pool.ntp.org"); // configure time to UTC+0
+
+    websocket_begin(); // initialize api connection
 }
 
-void loop() {
+void loop() 
+{
+
 }
